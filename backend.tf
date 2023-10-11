31448fc693b06e4bbb05858b7d6ada051670e647
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-10-11 15:19:26 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,6 +6,5 @@ terraform {
     region                      = "us-east-1"
     skip_requesting_account_id  = true
     skip_credentials_validation = true
-    skip_metadata_api_check     = true
   }
 }
