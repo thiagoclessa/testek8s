280f12abdba48d50a1a405ff6e09af0b8faf583d
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-10-11 15:08:10 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
-    endpoints.s3 = "https://nyc3.digitaloceanspaces.com"
-    region = "us-east-1"
-    key = "teste_k8s_do.tfstate"
-    bucket = "vkpr-state"
-    access_key="DO00RMBFWTK82WNF4AHA"
-    secret_key="uQgghONo3IcYzQlU07bQf8zooAMYGu+27HMDYRl/JDQ"
+    endpoint                    = ""https://nyc3.digitaloceanspaces.com""
+    key                         = "testek8s.tfstate"
+    bucket                      = "vkpr-state"
+    region                      = "us-east-1"
+    skip_requesting_account_id  = true
     skip_credentials_validation = true
-    skip_metadata_api_check = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
   }
 }
