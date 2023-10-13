2706448e25c6a8444b113b07ae44c58c1634d088
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-10-13 11:30:39 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = ""veecode""
+    organization = "veecode"
 
     workspaces {
-      name = ""veecode""
+      name = "veecode"
     }
   }
 }
