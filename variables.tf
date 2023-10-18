55b2a909eb3ae7b7798fdba4b77c9c346a6bc84c
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-10-18 11:08:21 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
 variable "do_token" {
-    default = "dop_v1_ce020afa8f64283593280ceb6aaf58693455db487deba196b6e71b61a9fb73a8"
+    default = "dop_v1_731b8558018a450a965fd3352e56e9fa0789691401d4e297e5668757b36b874b"
 }
